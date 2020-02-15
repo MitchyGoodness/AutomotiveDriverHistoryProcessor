f58ed8693484f267b84388a14e62f94cb064aa1a
MitchyGoodness <mitchelskees@gmail.com>
2020-02-14 22:28:05 -0500

    Add spec for Trip class

--- src/test/groovy/unit/TripSpec.groovy
+++ src/test/groovy/unit/TripSpec.groovy
@@ -1,5 +1,32 @@
-//import spock.lang.Specification
+import automotivedriverhistoryprocessor.Trip
+import spock.lang.Specification
 
-class TripSpec { //extends Specification {
+import java.time.LocalTime
 
+class TripSpec extends Specification {
+
+    void "Average speed is 10 mph"() {
+        given: "Trip of 20 miles driven in 2 hours"
+        LocalTime startTime = LocalTime.of(0, 0)
+        LocalTime endTime = LocalTime.of(1, 0)
+        Double distanceInMiles = 20
+
+        when: "Recording the trip"
+        Trip trip = new Trip(startTime, endTime, distanceInMiles)
+
+        then: "Average speed is 10 mph"
+        trip.getAverageSpeed() == 10
+    }
+
+    void "Speed is at minimum tolerance"() {
+        given: "Trip of 5 miles is driven in 1 hour"
+        LocalTime startTime = LocalTime.of(0, 0)
+        LocalTime endTime = LocalTime.of(1, 0)
+
+        when: "Recording the trip"
+        Trip trip = new Trip(startTime, endTime, 5)
+
+        then: "Speed is above minimum tolerance"
+        trip.isSpeedAboveMinTolerance()
+    }
 }
