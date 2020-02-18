8efec3dfb135bef94f002566fc1a208f062555bb
MitchyGoodness <mitchelskees@gmail.com>
2020-02-17 20:10:27 -0500

    Fix broken unit test

--- src/test/groovy/unit/TripSpec.groovy
+++ src/test/groovy/unit/TripSpec.groovy
@@ -7,6 +7,32 @@ import spock.lang.Unroll
 import java.time.LocalTime
 
 class TripSpec extends Specification {
+
+    void "MINIMUM_SPEED_TOLERANCE is not null"() {
+        expect:
+        Trip.MINIMUM_SPEED_TOLERANCE
+    }
+
+    void "MAXIMUM_SPEED_TOLERANCE is not null"() {
+        expect:
+        Trip.MAXIMUM_SPEED_TOLERANCE
+    }
+
+    void "constructor populates startTime, endTime, and distance"() {
+        given:
+        LocalTime startTime = LocalTime.of(0, 0)
+        LocalTime endTime = LocalTime.of(1, 0)
+        Integer distance = 5
+
+        when: 'creating trip object'
+        Trip trip = new Trip(startTime, endTime, distance)
+
+        then: 'startTime, endTime, and distance are populated correctly'
+        trip.startTime == startTime
+        trip.endTime == endTime
+        trip.distance == distance
+    }
+
     @Unroll
     void "trip.getAverageSpeed returns #averageSpeed mph when distance is #distance miles and duration is #duration minutes"() {
         given:
@@ -23,6 +49,7 @@ class TripSpec extends Specification {
         0        | 10       | 0
     }
 
+    @Unroll
     void "trip.isSpeedWithinAcceptedTolerances returns #withinTolerances for average speed #averageSpeed mph"() {
         given:
         LocalTime startTime = LocalTime.of(0, 0)
@@ -38,8 +65,8 @@ class TripSpec extends Specification {
         duration | distance | averageSpeed | aboveMinTolerance | belowMaxTolerance | withinTolerances
         90       | 15       | 10           | true              | true              | true
         60       | 5        | 5            | true              | true              | true
-        60       | 4.9      | 4.9          | false             | true              | false
+        60       | 4        | 4            | false             | true              | false
         60       | 100      | 100          | true              | true              | true
-        60       | 100.1    | 100.1        | true              | false             | false
+        60       | 101      | 101          | true              | false             | false
     }
 }
