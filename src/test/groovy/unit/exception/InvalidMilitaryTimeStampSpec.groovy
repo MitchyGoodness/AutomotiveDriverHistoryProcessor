af79c6b13ccc3bf9ac15e14d7235a3c5e7fb7d75
MitchyGoodness <mitchelskees@gmail.com>
2020-02-18 22:11:46 -0500

    Add InvalidMilitaryTimeStamp

---
+++ src/test/groovy/unit/exception/InvalidMilitaryTimeStampSpec.groovy
@@ -0,0 +1,24 @@
+package unit.exception
+
+import exception.InvalidMilitaryTimeStamp
+import spock.lang.Specification
+
+class InvalidMilitaryTimeStampSpec extends Specification {
+    void "default constructor initializes correctly"() {
+        given: 'calling constructor'
+        Exception e = new InvalidMilitaryTimeStamp()
+
+
+        expect: 'exception is initialized correctly'
+        e.message == null
+    }
+
+    void "constructor initializes correctly"() {
+        given: 'calling constructor'
+        Exception e = new InvalidMilitaryTimeStamp("Message")
+
+
+        expect: 'exception is initialized correctly'
+        e.message == "Message"
+    }
+}
