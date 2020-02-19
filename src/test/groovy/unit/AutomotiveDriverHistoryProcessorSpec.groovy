d4ffbb36ed13515c4bcbfb32b8815b2fd53a1989
MitchyGoodness <mitchelskees@gmail.com>
2020-02-18 22:02:44 -0500

    Finish unit tests for AutomotiveDriverHistoryProcessor

--- src/test/groovy/unit/AutomotiveDriverHistoryProcessorSpec.groovy
+++ src/test/groovy/unit/AutomotiveDriverHistoryProcessorSpec.groovy
@@ -1,11 +1,9 @@
 package unit
 
-import automotivedriverhistoryprocessor.AutomotiveDriver
 import automotivedriverhistoryprocessor.AutomotiveDriverHistory
 import automotivedriverhistoryprocessor.AutomotiveDriverHistoryProcessor
-import automotivedriverhistoryprocessor.Trip
 import exception.InvalidAutomotiveDriverLogInput
-import spock.lang.Ignore
+import exception.InvalidMilitaryTimeStamp
 import spock.lang.Specification
 import spock.lang.Unroll
 
@@ -142,38 +140,51 @@ class AutomotiveDriverHistoryProcessorSpec extends Specification {
         e.message == $/Command "Driver" expects 1 parameter got: ./$
     }
 
-    @Ignore
-    void "addTrip calls history.addTrip"() {
-//        given:
-//        String driverName = 'Elijah'
-//        LocalTime startTime = '00:00'
-//        LocalTime endTime = '01:00'
-//        Double distance = 50.0
-//        List<String> args = ['Trip', driverName, startTime, endTime, '50']
-//        AutomotiveDriverHistoryProcessor processor = new AutomotiveDriverHistoryProcessor(null)
-//
-//        and: "stubbed parseTime(String timeType, String timeStamp)"
-//        processor.metaClass.parseTime = { String timeType, List<String> parseTimeInput ->
-//            actualResult = addTripInput
-//        } as Closure<Void>
-//
-//        and: "stubbed parseDistance(String distance)"
-//        processor.metaClass.parseDistance = { List<String> parseDistanceInput ->
-//            actualResult = addTripInput
-//        } as Closure<Void>
-//
-//        and: "stubbed history.addTrip()"
-//        List<String> actualResult
-//        processor.history.metaClass.addTrip = { List<String> addTripInput ->
-//            actualResult = addTripInput
-//        } as Closure<Void>
-//
-//        when:
-//        processor.addDriver(args)
-//
-//        then:
-//        actualResult
-//        actualResult == driverName
+    void "addTrip calls several parsing functions and passes result"() {
+        given: 'Expected input'
+        String driverName = 'Elijah'
+        LocalTime startTime = LocalTime.of(0, 0)
+        LocalTime endTime = LocalTime.of(1, 0)
+        Double distance = 50.0
+
+        List<String> args = ['Trip', driverName, "$startTime", "$endTime", "$distance"]
+        AutomotiveDriverHistoryProcessor processor = new AutomotiveDriverHistoryProcessor(null)
+
+        and: "stubbed parseTime(String timeType, String timeStamp)"
+        Map parseTimeCallOutput = [:]
+        processor.metaClass.parseTime = { String timeType, String parseTimeInput ->
+            parseTimeCallOutput.put(timeType, parseTimeInput)
+            return timeType == 'start time' ? startTime : endTime
+        } as Closure<LocalTime>
+
+        and: "stubbed parseDistance(String distance)"
+        String parseDistanceOutput
+        processor.metaClass.parseDistance = { String parseDistanceInput ->
+            parseDistanceOutput = parseDistanceInput
+            return distance
+        } as Closure<Double>
+
+        and: "stubbed history.addTrip()"
+        List historyAddTripInput
+        processor.history.metaClass.addTrip = { String arg1, LocalTime arg2, LocalTime arg3, Double arg4 ->
+            historyAddTripInput = [arg1, arg2, arg3, arg4]
+        } as Closure<Void>
+
+        when: 'Adding a trip'
+        processor.addTrip(args)
+
+        then: 'parseTime is passed the start time and end time with matching identifier'
+        parseTimeCallOutput['start time'] == "$startTime"
+        parseTimeCallOutput['end time'] == "$endTime"
+
+        and: 'parseDistance is passed the distance'
+        parseDistanceOutput == "$distance"
+
+        and: 'history.addTrip() is passed all args in the correct order'
+        historyAddTripInput[0] == driverName
+        historyAddTripInput[1] == startTime
+        historyAddTripInput[2] == endTime
+        historyAddTripInput[3] == distance
     }
 
     void "addTrip handles bad input by throwing exception"() {
@@ -186,5 +197,70 @@ class AutomotiveDriverHistoryProcessorSpec extends Specification {
         then:
         Exception e = thrown(InvalidAutomotiveDriverLogInput)
         e.message == $/Command "Trip" expects 4 parameter got: ./$
+    }
+
+    @Unroll
+    void "parseTime parses valid input for #timeType correctly"() {
+        given:
+        AutomotiveDriverHistoryProcessor processor = new AutomotiveDriverHistoryProcessor(null)
+        LocalTime actualTime = processor.parseTime(timeType, timeStamp)
+
+        expect:
+        actualTime.getHour() == hour
+        actualTime.getMinute() == minute
+
+        where:
+        timeType     | timeStamp | hour | minute
+        'start time' | "01:30"   | 1    | 30
+        'end time'   | "05:24"   | 5    | 24
+    }
+
+    void "parseTime throws InvalidAutomotiveDriverLogInput exception for missing timestamp"() {
+        given:
+        AutomotiveDriverHistoryProcessor processor = new AutomotiveDriverHistoryProcessor(null)
+
+        when:
+        processor.parseTime('start time', null)
+
+        then: 'Exception is thrown'
+        Exception e = thrown(InvalidAutomotiveDriverLogInput)
+        e.message == 'Missing input "start time"'
+    }
+
+    void "parseTime throws InvalidMilitaryTimeStamp exception for invalid timestamp"() {
+        given:
+        AutomotiveDriverHistoryProcessor processor = new AutomotiveDriverHistoryProcessor(null)
+
+        when:
+        processor.parseTime('end time', "35:11")
+
+        then: 'Exception is thrown'
+        Exception e = thrown(InvalidMilitaryTimeStamp)
+        e.message == 'Expected end time in kk:mm (military time) format, got "35:11".'
+    }
+
+    void "parseDistance parses valid input correctly"() {
+        given:
+        AutomotiveDriverHistoryProcessor processor = new AutomotiveDriverHistoryProcessor(null)
+
+        when:
+        processor.parseDistance(null)
+
+        then: 'Exception is thrown'
+        Exception e = thrown(InvalidAutomotiveDriverLogInput)
+        e.message == 'Missing input "distance" for trip log.'
+    }
+
+    void "parseDistance throws exception for invalid input"() {
+        given:
+        String badDistanceInput = "a50.6"
+        AutomotiveDriverHistoryProcessor processor = new AutomotiveDriverHistoryProcessor(null)
+
+        when:
+        processor.parseDistance(badDistanceInput)
+
+        then: 'Exception is thrown'
+        Exception e = thrown(InvalidAutomotiveDriverLogInput)
+        e.message == $/Expected double or integer format for trip distance, got "$badDistanceInput"./$
     }
 }
