3eaa1b2e6ab0a24e43b22d4c21cb1962ec9e7662
MitchyGoodness <mitchelskees@gmail.com>
2020-02-18 23:49:12 -0500

    Added InputHandlerFactorySpec

---
+++ src/test/groovy/integration/io/InputHandlerFactorySpec.groovy
@@ -0,0 +1,41 @@
+package integration.io
+
+import io.FileHandler
+import io.InputHandlerFactory
+import io.StdinHandler
+import spock.lang.Specification
+
+class InputHandlerFactorySpec extends Specification {
+    void "constructor initializes and validates correctly for good file path"() {
+        given:
+        String[] args = ["non-empty input"]
+
+        when: 'calling constructor'
+        InputHandlerFactory inputHandlerFactory = new InputHandlerFactory(args)
+
+        then: 'inputHandlerFactory.args are initialized with provided input args'
+        inputHandlerFactory.args == args
+    }
+
+    void "getInputHandler returns FileHandler when file path is provided"() {
+        given:
+        String[] args = ["src/test/resources/AutomotiveDriverInput.txt"]
+
+        when:
+        InputHandlerFactory inputHandlerFactory = new InputHandlerFactory(args)
+
+        then:
+        inputHandlerFactory.getInputHandler().class == FileHandler
+    }
+
+    void "getInputHandler returns Stdin when no file path is provided"() {
+        given:
+        String[] args = null
+
+        when:
+        InputHandlerFactory inputHandlerFactory = new InputHandlerFactory(args)
+
+        then:
+        inputHandlerFactory.getInputHandler().class == StdinHandler
+    }
+}
