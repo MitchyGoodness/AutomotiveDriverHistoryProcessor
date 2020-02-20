9f6643e62df3a15da1814f52cc7c9d09cf7f6972
MitchyGoodness <mitchelskees@gmail.com>
2020-02-19 21:04:12 -0500

    Add MainSpec

---
+++ src/test/groovy/integration/automotivedriverhistoryprocessor/MainSpec.groovy
@@ -0,0 +1,30 @@
+package integration.automotivedriverhistoryprocessor
+
+import automotivedriverhistoryprocessor.Main
+import spock.lang.Specification
+import spock.lang.Unroll
+
+class MainSpec extends Specification {
+    @Unroll
+    void "Take a file of inputs and print list of sorted drivers and their statistics"() {
+        given: "Test setup"
+        String filePath = "src/test/resources/${fileName}.txt"
+
+        ByteArrayOutputStream buffer = new ByteArrayOutputStream()
+        System.out = new PrintStream(buffer)
+
+        when: "processing file and getting driver history"
+        Main.main([filePath] as String[])
+
+        and: "actual output is formatted in the unix line return format"
+        String actualOutput = buffer.toString().replaceAll(/(\r\n|\r)/, "\n").replaceAll(/\n$/, "")
+
+        then: "Printed output matches expectation"
+        actualOutput == expectedOutput
+
+        where:
+        fileName                | expectedOutput
+        "AutomotiveDriverInput" | "mitch 90 miles @ 30 mph\nmark 49 miles @ 75 mph\ntina 0 miles"
+        "empty"                 | ""
+    }
+}
